c0f0848b04db4b7d4760c1fd3e77ccd671e9c6c2
David Good <davidagood@gmail.com>
2016-06-09 21:44:16 +0100

    fix typo in default search term

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term =  params[:looking_for] || 'pyhton'
+  	@search_term =  params[:looking_for] || 'python'
   	@courses = Coursera.for(@search_term)
   end
 end
